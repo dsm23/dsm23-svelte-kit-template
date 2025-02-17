06c8e57f39b4e994427a33ccf0e814fa1e5e5a97
David Murdoch <dsmurdoch357@gmail.com>
2025-02-17 00:24:53 +0000

    ci(unit testing): add unit testing pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,30 @@ import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 
 	test: {
+		coverage: {
+			all: true,
+			include: ['src/**/*.svelte', 'src/**/*.[jt]s?(x)'],
+			exclude: [
+				'src/**/*.stories.[jt]s?(x)',
+				'src/test-utils/**',
+				'src/mocks/**',
+				'**/node_modules/**',
+				'**/e2e/**',
+				...coverageConfigDefaults.exclude
+			]
+			// thresholds: {
+			// 	lines: 10,
+			// 	functions: 10,
+			// 	branches: 10,
+			// 	statements: 10
+			// }
+		},
 		workspace: [
 			{
 				extends: './vite.config.ts',
