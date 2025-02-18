c151889b14958099dcb582dcc3fee2bce38adb30
David Murdoch <dsmurdoch357@gmail.com>
2025-02-18 17:42:53 +0000

    chore(prettier): update prettier config
    * added @ianvs/prettier-plugin-sort-imports
    * added prettier-plugin-css-order
    * change prettier config filetype

---
+++ prettier.config.js
@@ -0,0 +1,43 @@
+/**
+ *  @type {import("prettier").Options}
+ */
+const config = {
+  plugins: [
+    "prettier-plugin-css-order",
+    "prettier-plugin-svelte",
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  cssDeclarationSorterOrder: "smacss",
+  importOrder: [
+    "^svelte$",
+    "<TYPES>^(svelte)",
+    "^clsx$",
+    "<TYPES>^(clsx)",
+    "^@*svelte",
+    "<TYPES>^(@*svelte)",
+    "<BUILTIN_MODULES>",
+    "<TYPES>^(node:)",
+    "<THIRD_PARTY_MODULES>",
+    "<TYPES>^([@a-z])",
+    "^~/(.*)$",
+    "<TYPES>^~/(.*)",
+    "^[.]",
+    "<TYPES>",
+    "",
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
+  ],
+  importOrderTypeScriptVersion: "5.7.3",
+  overrides: [
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+  ],
+  tailwindFunctions: ["clsx", "cn", "cva", "cx", "tv"],
+};
+
+export default config;
