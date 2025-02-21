b7617fa1cc44b377c127986470787f980b8aae6a
David Murdoch <dsmurdoch357@gmail.com>
2025-02-21 20:05:35 +0000

    feat(dark mode): add dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  import DarkMode from "~/components/dark-mode/index.svelte";
+
   import "~/app.css";
+
+  import { ModeWatcher } from "mode-watcher";
 
   let { children } = $props();
 </script>
 
+<ModeWatcher />
+
 {@render children()}
+<footer
+  class="mx-auto flex w-full items-center justify-center border-t py-16 text-center text-xs"
+>
+  <DarkMode />
+</footer>
